baf58ff17dabc158384f211ff0343ab4cf0a02f6
Zero Zhang <southbean2013@gmail.com>
2017-06-26 13:12:03 +0800

    add stop, left, right action

--- js/index.js
+++ js/index.js
@@ -3,15 +3,17 @@ var board = document.getElementById('board'),
 		stopBtn = document.getElementById('btn_stop'),
 		endBtn = document.getElementById('btn_end'),
 		ctx = board.getContext('2d');
-		ctx.strokeStyle = "#BFBFBF";
+var raf = null,
+		timer = null;
 
 /*
  * 绘制场地
  */
 var Painter = {
-	init: function(w, h) {
+	view: function(w, h) {
 		var col = w / 20,
 				row = h / 20;
+		ctx.strokeStyle = '#BFBFBF';
 		for(var i = 0; i <= row; i++) {
 			ctx.moveTo(0, i*20);
 			ctx.lineTo(w, i*20);
@@ -26,24 +28,25 @@ var Painter = {
 }
 
 var Snake = {
-	x: 0,
+	x: -20,
 	y: 0,
-	length: 2,
+	len: 2,
 	vx: 1,
 	vy: 0,
 	view: function() {
 		ctx.fillStyle = '#000';
-		ctx.fillRect(this.x, this.y, this.length * 20, 20);
+		ctx.fillRect(this.x + this.vx * 20, this.y, this.len * 20, 20);
+		this.x += this.vx * 20;
 	}
 }
 
 function move() {
-	ctx.clearRect(Snake.x, Snake.y, Snake.length * 20, 20);
-	Snake.x += 20;
+	ctx.clearRect(0, 0, 600, 400);
+	Painter.view(600, 400);
 	Snake.view();
-	setTimeout(function() {
-		window.requestAnimationFrame(move);
-	}, 1000);
+	timer = setTimeout(function() {
+		raf = window.requestAnimationFrame(move);
+	}, 800);
 }
 
 
@@ -53,13 +56,42 @@ var page = {
 		this.listen();
 	},
 	view: function() {
-		Painter.init(600, 400);
+		Painter.view(600, 400);
 		Snake.view();
 	},
 	listen: function() {
 		startBtn.addEventListener('click', function() {
 			move();
 		}, false);
+		stopBtn.addEventListener('click', function() {
+			// window.cancelAnimationFrame(raf);
+			clearTimeout(timer);
+		}, false);
+		endBtn.addEventListener('click', function() {
+			clearTimeout(timer);
+			alert('Game over!');
+		}, false);
+		window.addEventListener('keydown', function(e) {
+			var key = e.keyCode || e.which;
+			switch(key) {
+				case 37: //左
+					if(Snake.vx > 0) {
+						Snake.vx = -Snake.vx;
+					}
+					break;
+				case 38: //上
+					break;
+				case 39: //右
+					if(Snake.vx < 0) {
+						Snake.vx = -Snake.vx;
+					}
+					break;
+				case 40: //下
+					break;
+				default:
+					;
+			}
+		})
 	}
 }
 
